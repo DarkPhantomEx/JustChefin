dfc0b041539f10c651bab9bf99167c41280b8575
Joshua Issac (RIT Student) <ji5780@rit.edu>
2021-02-15 19:05:00 -0500

    Basic FileIO Implementation
    Not yet tested, as it needs to be incorporated with the pertinent GameObject.

---
+++ JustChefin/Assets/Scripts/FileIO.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public static class FileIO
+{
+    public static void SaveRecipeData(RecipeSystem Recette)
+    {
+        BinaryFormatter bF = new BinaryFormatter();
+        //Creates a "save file" in the User's folder under the name of recette.jc
+        FileStream file = new FileStream(Application.persistentDataPath + "/recette.jc", FileMode.Create);
+
+        RecipeData data = new RecipeData(Recette);
+
+        bF.Serialize(file, data);
+        file.Close();
+    }
+
+    public static void LoadRecipeData(ref RecipeData data)
+    {
+        if(File.Exists(Application.persistentDataPath + "/recette.jc"))
+        {
+            BinaryFormatter bF = new BinaryFormatter();
+            //Opens the save file in the User's folder under the name of recette.jc
+            FileStream file = new FileStream(Application.persistentDataPath + "/recette.jc", FileMode.Open);
+
+            //Deserializes the saved recette.jc file
+            data = (RecipeData)bF.Deserialize(file);
+            file.Close();
+        }
+        else
+        {
+            Debug.LogError("File Doesn't Exist, bud.");           
+        }
+    }
+
+}
+
+[SerializeField]
+public class RecipeData
+{
+    //A class that contains all the pertinent data from RecipeSystem.cs This data will be stored in our save file "recette.jc"
+
+    public List<int> locID;
+    public List<string> Instr;
+    public List<int> timer;
+    public int num;
+
+
+    public RecipeData(RecipeSystem Recette)
+    {
+        this.locID = new List<int>(Recette.locID);
+        this.Instr = new List<string>(Recette.Instr);
+        this.timer = new List<int>(Recette.timer);
+        this.num = Recette.numInstr;
+    }
+
+}
+
--- JustChefin/Assets/Scripts/RecipeSystem.cs
+++ JustChefin/Assets/Scripts/RecipeSystem.cs
@@ -21,12 +21,35 @@ public class RecipeSystem : MonoBehaviour
     [SerializeField]
     Timer recipeTimer;
 
+    public int numInstr;
+    public List<int> locID;    
+    public List<string> Instr;    
+    public List<int> timer;
+
     // Start is called before the first frame update
     void Start()
     {
+        numInstr = 0;
         recipeTimer = this.gameObject.GetComponent<Timer>();
         
     }
+    
+    public void Save()
+    {
+        //Stores the List of recipes into recette.jc, by calling FileIO's save method
+        FileIO.SaveRecipeData(this);
+    }
+
+    public void Load(RecipeData Recette)
+    {
+        //Loads the List of recipes into recette.jc, by calling FileIO's save method
+        FileIO.LoadRecipeData(ref Recette);
+        this.locID = new List<int>(Recette.locID);
+        this.Instr = new List<string>(Recette.Instr);
+        this.timer = new List<int>(Recette.timer);
+        this.numInstr = Recette.num;
+    }
+
 
     // Update is called once per frame
     void Update()
