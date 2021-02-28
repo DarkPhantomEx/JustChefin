18a9ea7aabe98559207ea324b4ff1819ee04dfff
Abhishek Panhale <ap8093@rit.edu.in>
2021-02-27 21:18:02 -0500

    Implemented player movement along with a follow camera

---
+++ JustChefin/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    public float smoothFactor = 10f;
+    private Vector3 cameraOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraOffset = transform.position - player.transform.position;
+    }
+
+    // LateUpdate is called once per frame
+    void LateUpdate()
+    {
+        //transform.position = player.transform.position + cameraOffset;
+
+        Vector3 newPosition = player.transform.position + cameraOffset;
+        transform.position = Vector3.Slerp(transform.position, newPosition, smoothFactor);
+    }
+}
