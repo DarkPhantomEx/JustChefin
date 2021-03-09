1376568fec748c0afc8d87056918d49a010dbb57
Joshua Issac (RIT Student) <ji5780@rit.edu>
2021-03-08 21:08:22 -0500

    MainMenu
    Added basic functionality + tutorial

---
+++ JustChefin/Assets/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+    public void EndGame()
+    {
+        Debug.Log("It's the end of the game as we know it.");
+        Application.Quit();
+    }
+}
