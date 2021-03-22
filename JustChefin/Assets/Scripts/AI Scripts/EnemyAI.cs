9784c079e5adcdd2df173dfb36a08bc08d379f2f
Abhishek Panhale <ap8093@rit.edu.in>
2021-03-22 02:40:04 -0400

    Added a suspicion bar for enemy AI

--- JustChefin/Assets/Scripts/AI Scripts/EnemyAI.cs
+++ JustChefin/Assets/Scripts/AI Scripts/EnemyAI.cs
@@ -18,7 +18,13 @@ public class EnemyAI : MonoBehaviour
     public float idleTime;
     public GameObject[] waitPoints;
 
-    [SerializeField]
+    // Suspicion bar variables
+    public float minSuspicionDistance;
+    public SuspicionBar suspicionBar;
+    private float playerDistance;
+    private static float currentSuspicionValue;
+
+    // For going through the waitpoints sequentially
     private int waitPointIterator;
 
     // Reference to PlayerStatus script to be used in individual state classes
@@ -33,6 +39,11 @@ public class EnemyAI : MonoBehaviour
         tdmScript = GameObject.Find("TopDownPlayer").GetComponent<TopDownMovement>();
 
         waitPointIterator = 0;
+        playerDistance = Vector3.Distance(this.transform.position, player.transform.position);
+
+        // Default values for suspicion bar
+        currentSuspicionValue = 0f;
+        suspicionBar.SetMinSuspicion(GetMinSuspicionValue());
 
         // Set default start state as Patrol
         ChangeState(new PatrolState(this));
@@ -42,7 +53,34 @@ public class EnemyAI : MonoBehaviour
     void Update()
     {
         currentState.UpdateState();
-        Debug.DrawRay(this.transform.position, this.transform.forward * 6f, Color.yellow);
+        //Debug.DrawRay(this.transform.position, this.transform.forward * 6f, Color.yellow);
+
+        // Calculate distance between the player and the enemy
+        playerDistance = Vector3.Distance(this.transform.position, player.transform.position);
+
+        // If raycast from enemy to the player is successful
+        if(IsRaycastToPlayerSuccess())
+        {
+            switch (this.tag)
+            {
+                // If Agro then update suspicion bar
+                case "Agro":
+                    UpdateSuspicionBar();
+                    break;
+                // If Passive and player has recipe, then update suspicion bar
+                case "Passive":
+                    if (this.psScript.GetHasRecipe())
+                    {
+                        UpdateSuspicionBar();
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        // Reset suspicion bar if player is behind any object
+        if (IsRaycastToPlayerFailed())
+            suspicionBar.SetMinSuspicion(GetMinSuspicionValue());
     }
 
     // Function to change the state
@@ -92,7 +130,36 @@ public class EnemyAI : MonoBehaviour
         return false;
     }
 
+    // Function to check if raycast is NOT possible from enemy's location to player's location
+    public bool IsRaycastToPlayerFailed()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(this.transform.position, player.transform.position - this.transform.position, out hit, Mathf.Infinity))
+        {
+            if (hit.collider.gameObject != player)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Function to calculate suspicion bar value
+    public void UpdateSuspicionBar()
+    {
+        // Detemine the value of suspicion bar
+        if (playerDistance <= minSuspicionDistance)
+        {
+            currentSuspicionValue = 1 - ((playerDistance - sightRange) / (minSuspicionDistance - sightRange));
+            suspicionBar.SetSuspicion(currentSuspicionValue);
+        }
+    }
+
+    // Getter and Updaters for waitPointIterator variable
     public int GetWaitPointIterator() { return waitPointIterator; }
     public void IncrementWaitPointIterator() { waitPointIterator++; }
     public void ResetWaitPointIterator() { waitPointIterator = 0; }
+
+    // Getter for the minimum value of the suspicion bar
+    public float GetMinSuspicionValue() { return suspicionBar.slider.minValue; }
 }
